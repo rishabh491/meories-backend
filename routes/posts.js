c95163d0b9286a19e052ee50fb646a8c1033d145
rishabh491 <rishabhsahani491@gmail.com>
2024-05-25 15:07:25 +0530

    backend

---
+++ routes/posts.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+  getPost,
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  commentPost,
+  getPostsBySearch,
+} from "../controllers/post.js";
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
+
+export default router;
